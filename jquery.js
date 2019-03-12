9e55585969fd61d54f2e59ed72806e17d6745b2b
Your Name <you@example.com>
2019-03-12 16:03:11 -0400

    added githum icons and job titles to about page

--- jquery.js
+++ jquery.js
@@ -3,7 +3,7 @@
 
 $(window).scroll(function() {
   var div = $("#divToShowHide");
-  if($(window).scrollTop() > 1000) {
+  if($(window).scrollTop() > 900) {
     div.addClass("AfterScroll");;
   } else {
     div.removeClass("AfterScroll")
